90b4171803b8bfab50f5008e74d55fa127cce4b5
jawoletz <josephwoletz@gmail.com>
2023-03-08 20:25:05 -0800

    Characters appear on display when button clicked

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function operate(operator, x, y) {
         case "/":
             divide(x, y);
             break;
-        case "*":
+        case "x":
             multiply(x, y);
             break;
         default:
@@ -32,3 +32,37 @@ function divide(x, y) {
 function multiply(x, y) {
     return x * y;
 }
+
+const equation = [];
+
+// Get elements
+const display = document.getElementById('display');
+const clear = document.getElementById('clear');
+const equals = document.getElementById('equals');
+
+// Show input on display
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        equation.push(button.textContent);
+        display.textContent = equation.join('');
+    });
+});
+
+// Clear the display
+clear.addEventListener('click', function() {
+    equation.length = 0;
+    display.textContent = '';
+});
+
+// Solve the equation
+equals.addEventListener('click', function() {
+    console.log(equation);
+    let x = equation.find(char => char == re);
+    // let operator = equation.find(char => 
+    console.log(x);
+});
+
+// Make solve
+// Accepts numbers larger than 9
+// 
